187c27950c3f4976b545ec74e52ceb72156a258c
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-06-05 20:45:29 -0300

    fix: ajusts inproduct details to make funtional

--- src/app/produto/[id]/page.tsx
+++ src/app/produto/[id]/page.tsx
@@ -1,7 +1,11 @@
 // Components
 import FooterComponent from '@/components/footer/Footer-component'
-import HeaderComponent from '@/components/Header-component'
+import HeaderComponent from '@/components/header/Header-component'
 import ButtonComponent from '@/components/button/Button-component'
+import MyimgComponent from '@/components/imgcomponent/Myimg-component'
+import HeartIcon from '@heroicons/react/24/outline/HeartIcon'
+import CartIcon from '@heroicons/react/24/outline/ShoppingCartIcon'
+
 // Utilities
 import { Produtos } from '../../../../script/Products'
 
@@ -10,59 +14,57 @@ export default function ProductDetails({ params }: any) {
   return (
     <>
       <HeaderComponent />
-
-      <div className=" w-full">
-        <div className="m-auto h-20 w-[80vw]">
-          <h1>Voltar</h1>
-        </div>
-
+      <div className="h-6" />
+      <div className="m-auto h-16 w-[100vw] md:w-[80vw]  lg:w-[80vw]"></div>
+      <div className=" m-auto  w-[100vw] md:w-[80vw]  lg:w-[80vw] ">
         {Produtos.filter((produtos) => produtos.id == idParams).map(
           (produto) => (
             <div className="m-auto flex w-[80vw] flex-col items-center">
               <div className="w-[80vw ]">
-                <div className="m-auto h-[300px] w-[400px]">
-                  <img
-                    src={produto.imagem}
-                    alt={`imagem do ${produto.nome}`}
-                    className="h-full w-full"
-                  />
+                <div className="relative m-auto h-[300px] w-[400px] rounded-md bg-customgray">
+                  <HeartIcon className="absolute right-2 top-2 w-5 cursor-pointer text-white" />
+                  <MyimgComponent alt={produto.nome} src={produto.imagem} />
                 </div>
-                <div className="my-4 flex h-20 cursor-pointer gap-5 ">
-                  <div className="flex-1 bg-customgray bg-customgray opacity-80">
-                    <img
-                      src={produto.imagem}
-                      alt={`imagem do ${produto.nome}`}
-                      className="h-full w-full "
-                    />
+                <div className="my-4 flex h-20  w-[400px] gap-4 ">
+                  <div className="flex-1 rounded-md bg-customgray opacity-80">
+                    <MyimgComponent alt={produto.nome} src={produto.imagem} />
                   </div>
-                  <div className="flex-1 cursor-pointer bg-customgray opacity-70">
-                    <img
-                      src={produto.imagem}
-                      alt={`imagem do ${produto.nome}`}
-                      className="h-full w-full"
-                    />
+                  <div className="flex-1 cursor-pointer rounded-md bg-customgray opacity-70">
+                    <MyimgComponent alt={produto.nome} src={produto.imagem} />
                   </div>
-                  <div className="flex-1 cursor-pointer bg-customgray opacity-70">
-                    {' '}
-                    <img
-                      src={produto.imagem}
-                      alt={`imagem do ${produto.nome}`}
-                      className="h-full w-full"
-                    />
+                  <div className="flex-1 cursor-pointer rounded-md bg-customgray opacity-70">
+                    <MyimgComponent alt={produto.nome} src={produto.imagem} />
                   </div>
-                  <div className="flex-1 cursor-pointer bg-customgray opacity-70">
-                    <img
-                      src={produto.imagem}
-                      alt={`imagem do ${produto.nome}`}
-                      className="h-full w-full"
-                    />
+                  <div className="flex-1 cursor-pointer rounded-md bg-customgray opacity-70">
+                    <MyimgComponent alt={produto.nome} src={produto.imagem} />
                   </div>
                 </div>
               </div>
-              <h1>{produto.nome}</h1>
-              <p> descrição {produto.nome}</p>
-              <p>mais descriçoes</p>
-              <ButtonComponent>Adicionar ao carrinho</ButtonComponent>
+              <div className="m-auto w-[400px] py-4 text-left">
+                <div className="flex items-center justify-between">
+                  <h1 className="text-3xl font-bold">{produto.nome}</h1>
+                  <div className="flex items-end justify-end gap-2">
+                    <p className="text-xs text-customgray">
+                      Vendidos {produto.vendidos}
+                    </p>
+                    <p className="text-xs text-customgray"> | </p>
+                    <p className="text-xs text-customgray">
+                      Estoque {produto.estoque}
+                    </p>
+                  </div>
+                </div>
+                <p>Mais informações sobre o produto</p>
+                <div className="my-4 h-2 w-8 rounded-md  bg-customgray" />
+                <p className="font-extralight line-through">
+                  de ${Math.floor(produto.preço * 2 + produto.preço / 3)},99
+                </p>
+                <h3 className="text-lg font-bold">R${produto.preço}</h3>
+              </div>
+              <div className="flex w-[400px] items-end justify-end">
+                <ButtonComponent icon={<CartIcon width={20} />}>
+                  Adicionar ao carrinho
+                </ButtonComponent>
+              </div>
             </div>
           )
         )}
