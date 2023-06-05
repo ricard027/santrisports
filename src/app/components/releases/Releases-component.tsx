20820dab17c2b92a0adab69ccfbc2548bef0613f
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-06-04 23:26:28 -0300

    feat: initial ajusts to releasescomponent

--- src/app/components/releases/Releases-component.tsx
+++ src/app/components/releases/Releases-component.tsx
@@ -1,28 +1,28 @@
+//  Components
 import CarouselComponent from '../Carousel-component'
+//  Utilities
+import { SwiperSlide } from 'swiper/react'
 
 const ReleasesComponent = () => {
   return (
-    <section className="flex justify-evenly gap-4">
-      <div className="flex-1 cursor-pointer">
-        <div className="flex h-48 flex-col items-center justify-center rounded-full bg-customgray">
-          <p>Acessar</p>
-        </div>
-      </div>
-      <div className="flex-1 cursor-pointer">
-        <div className="flex h-48 flex-col items-center justify-center rounded-full bg-customgray">
-          <p>Acessar</p>
-        </div>
-      </div>
-      <div className="flex-1 cursor-pointer">
-        <div className="flex h-48 flex-col items-center justify-center rounded-full bg-customgray">
-          <p>Acessar</p>
-        </div>
-      </div>
-      <div className="flex-1 cursor-pointer">
-        <div className="flex h-48 flex-col items-center justify-center rounded-full bg-customgray">
-          <p>Acessar</p>
-        </div>
-      </div>
+    <section className="w-full">
+      <CarouselComponent
+        iwantNavigateorNot={true}
+        iWantPaginationorNot={false}
+        autoplayornot={false}
+        lg={4}
+        md={4}
+        sm={3}>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+          <SwiperSlide key={item}>
+            <div className="flex h-36 w-36  cursor-pointer flex-col items-center justify-center rounded-full bg-customgray">
+              <div className=" ">
+                <p>Acessar</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </CarouselComponent>
     </section>
   )
 }
