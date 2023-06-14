9067a7af2ae44a08d215af9b42a0220554d1e688
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-06-14 17:41:03 -0300

    fix: styles changed to make funtional

--- src/components/cart/Cart-component.tsx
+++ src/components/cart/Cart-component.tsx
@@ -9,8 +9,8 @@ const CartComponent = ({ setCartactive }: any) => {
         className="fixed left-0 top-0 z-40  h-full  w-[100vw]   bg-[rgb(0,0,0,.7)]"
         onClick={() => setCartactive((prev: boolean) => !prev)}
       />
-      <div className="fixed right-0 top-0 z-50 flex h-full  justify-end ">
-        <div className="relative z-50 flex h-full w-[80vw] flex-col justify-evenly bg-[#f0f0f0]  opacity-100 sm:w-[400px]">
+      <div className="fixed right-0 top-0 z-50 flex h-full  justify-end">
+        <div className="relative z-50 flex h-full w-[60vw] flex-col justify-evenly bg-[#f0f0f0]  opacity-100 sm:w-[350px]">
           {/** Cart Title */}
           <div className="m-auto w-[80%]">
             <h1 className="my-2 text-lg font-semibold">Carrinho</h1>
@@ -18,16 +18,17 @@ const CartComponent = ({ setCartactive }: any) => {
           </div>
           {/** Cart Title */}
           {/** Cart List*/}
-          <ul className="custom-scrollbar m-auto mb-6 h-96 w-[80%] overflow-y-scroll">
-            <li className="my-5 flex w-[100%] flex-auto items-center justify-start gap-4 rounded bg-white p-4 shadow-xl">
-              <div className=" h-16 w-12 flex-shrink-0">
+          <ul className="custom-scrollbar m-auto mb-6 h-96 w-[80%] overflow-y-scroll flex flex-col justify-start items-center">
+            <li className="my-2 flex w-full h-18 justify-evenly items-center p-3
+               gap-2 rounded bg-white  shadow-xl">
+              <div className="h-full w-12">
                 <img
                   src="https://source.unsplash.com/random/200x200/?sports"
                   alt="Tênis"
                   className="h-full w-full  object-cover"
                 />
               </div>
-              <div className="flex flex-1 flex-col">
+              <div className="flex flex-1 flex-col  w-[30%]">
                 <p className="truncate text-sm font-semibold text-gray-800">
                   Tênis de corrida
                 </p>
@@ -36,7 +37,7 @@ const CartComponent = ({ setCartactive }: any) => {
                   <p className="text-xs text-gray-600">R$ 199.99</p>
                 </div>
               </div>
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-1 ">
                 <div className="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-gray-200">
                   -
                 </div>
@@ -46,6 +47,35 @@ const CartComponent = ({ setCartactive }: any) => {
                 </div>
               </div>
             </li>
+            <li className="my-2  flex w-full h-18 justify-evenly items-center p-3
+               gap-2 rounded bg-white  shadow-xl">
+              <div className="h-full w-12">
+                <img
+                  src="https://source.unsplash.com/random/200x200/?sports"
+                  alt="Tênis"
+                  className="h-full w-full  object-cover"
+                />
+              </div>
+              <div className="flex flex-1 flex-col  w-[30%]">
+                <p className="truncate text-sm font-semibold text-gray-800">
+                  Tênis de corrida
+                </p>
+                <div className="my-2 h-[6px] w-6 rounded-md bg-[#D9D9D9]" />
+                <div className="flex items-center gap-2">
+                  <p className="text-xs text-gray-600">R$ 199.99</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-1  ">
+                <div className="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-gray-200">
+                  -
+                </div>
+                <div className="text-gray-800">1</div>
+                <div className="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-gray-200">
+                  +
+                </div>
+              </div>
+            </li>
+
           </ul>
           {/** Cart List*/}
           <div className="m-auto flex  w-[80%] flex-col items-start justify-start  gap-2 py-2">
